d1a0fa8b93d419546f856e9ad9a7f36de6649e92
Hanz C. Bande <hanz@example.com>
2026-04-21 09:24:45 +0800

    updated database and added error page

--- jeepneynvans.sql
+++ jeepneynvans.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2026 at 07:26 AM
+-- Generation Time: Apr 20, 2026 at 03:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
